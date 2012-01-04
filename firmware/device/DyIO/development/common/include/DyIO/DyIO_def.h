b1b5cccc6bf4742539384fbbae02b991d7ee1e21
Kevin Harrington <mad.hephaestus@gmail.com>
2012-01-04 18:35:36 +0000

    New publisher

--- firmware/device/DyIO/development/common/include/DyIO/DyIO_def.h
+++ firmware/device/DyIO/development/common/include/DyIO/DyIO_def.h
@@ -25,9 +25,9 @@
 
 #define NUM_PINS   			24
 
-#define MAJOR_REV			3
-#define MINOR_REV			7
-#define FIRMWARE_VERSION	19
+//#define MAJOR_REV			3
+//#define MINOR_REV			7
+//#define FIRMWARE_VERSION	19
 
 #define NO_CHANGE	 	 		0x00
 #define HIGH_IMPEDANCE 	 		0x01
