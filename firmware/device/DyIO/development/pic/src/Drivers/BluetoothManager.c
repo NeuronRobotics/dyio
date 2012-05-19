6514dc03f80b45ea3b8ddc7ce6cb24adde4d6283
Alex Camilo <alex.camilo@gmail.com>
2012-05-19 16:53:49 +0000

    added #defines for new bluetooth DyIO.

--- firmware/device/DyIO/development/pic/src/Drivers/BluetoothManager.c
+++ firmware/device/DyIO/development/pic/src/Drivers/BluetoothManager.c
@@ -4,20 +4,25 @@
  */
 #include "UserApp.h"
 
-BOOL btAvailible = FALSE;
-
 BYTE hasBluetooth(){
 	//TODO check for bluetooth module
-	btAvailible=TRUE;
-	return btAvailible;
+	return TRUE;
 }
+
+#define BluetoothResetTRIS		_TRISD1
+#define BluetoothCommandTRIS	_TRISD4
+#define BluetoothLinkTRIS		_TRISD6
+
+#define BluetoothReset			_RD1
+#define BluetoothCommand		_RD4
+#define BluetoothLink			_RD6
 
 
 
 void initBluetooth(){
-	_TRISD1=0; //output mode on reset line
-	_TRISD4=0; //output mode on CMD line
-	_RD1=1; // Pull BT module out of reset
+	BluetoothResetTRIS=0; //output mode on reset line
+	BluetoothCommandTRIS=0; //output mode on CMD line
+	BluetoothReset=1; // Pull BT module out of reset
 	mPORTDOpenDrainClose(BIT_2 | BIT_3); // make sure the com port is driven 3.3
 	//_RD4=1; // enter command mode. this doesn't work cause i re-used the pin for RD4. Sorry kevin
 
