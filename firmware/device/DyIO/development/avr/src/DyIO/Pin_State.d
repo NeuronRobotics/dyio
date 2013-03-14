2dd1d54f0306f18eb54cf3b738d44f11768a8066
Kevin Harrington <mad.hephaestus@gmail.com>
2013-03-14 15:57:40 +0000

    New Makefile system for AVD, full DyIO switched over to new Makefile system

---
+++ firmware/device/DyIO/development/avr/src/DyIO/Pin_State.d
@@ -0,0 +1,92 @@
+src/DyIO/Pin_State.o: src/DyIO/Pin_State.c include/UserApp.h \
+ ../NR-Clib/include/Bowler/Bowler.h ../NR-Clib/include/Bowler/Defines.h \
+ ../NR-Clib/include/Bowler/Bowler_Struct_Def.h \
+ ../NR-Clib/include/Bowler/Debug.h \
+ ../NR-Clib/include/Bowler/Bowler_Helper.h \
+ ../NR-Clib/include/Bowler/Bowler_Server.h \
+ ../NR-Clib/include/Bowler/FIFO.h ../NR-Clib/include/Bowler/RPC_Process.h \
+ ../NR-Clib/include/Bowler/Scheduler.h \
+ ../NR-Clib/include/Bowler/AbstractPID.h \
+ ../NR-Clib/include/Bowler/BowlerTransport.h \
+ ../NR-Clib/include/Bowler/namespace.h \
+ ../NR-Clib/include/Bowler/BowlerRPCRegestration.h \
+ ../NR-Clib/include/Bowler/Namespace_bcs_core.h \
+ ../NR-Clib/include/arch/AVR/BowlerConfig.h \
+ ../NR-Clib/include/arch/AVR/reg_structs.h \
+ ../NR-Clib/include/Bowler/BowlerServerHardwareAbstraction.h \
+ include/DyIOApp/HardwareProfile.h ../common/include/DyIO/DyIO_def.h \
+ ../common/include/DyIO/Debug_DyIO.h ../common/include/DyIO/ChannelMode.h \
+ ../common/include/DyIO/ChannelValues.h \
+ ../NR-Clib/include/Bowler/AbstractPID.h include/DyIOApp/Power.h \
+ include/DyIOApp/Servo.h include/DyIOApp/AVR_IO.h include/DyIOApp/ADC.h \
+ include/DyIOApp/PWM.h include/DyIOApp/EEPROM.h \
+ include/DyIOApp/UARTPassThrough.h include/DyIOApp/DCMotorDriver.h \
+ include/DyIOApp/Push.h include/DyIOApp/SPISlave.h
+
+include/UserApp.h:
+
+../NR-Clib/include/Bowler/Bowler.h:
+
+../NR-Clib/include/Bowler/Defines.h:
+
+../NR-Clib/include/Bowler/Bowler_Struct_Def.h:
+
+../NR-Clib/include/Bowler/Debug.h:
+
+../NR-Clib/include/Bowler/Bowler_Helper.h:
+
+../NR-Clib/include/Bowler/Bowler_Server.h:
+
+../NR-Clib/include/Bowler/FIFO.h:
+
+../NR-Clib/include/Bowler/RPC_Process.h:
+
+../NR-Clib/include/Bowler/Scheduler.h:
+
+../NR-Clib/include/Bowler/AbstractPID.h:
+
+../NR-Clib/include/Bowler/BowlerTransport.h:
+
+../NR-Clib/include/Bowler/namespace.h:
+
+../NR-Clib/include/Bowler/BowlerRPCRegestration.h:
+
+../NR-Clib/include/Bowler/Namespace_bcs_core.h:
+
+../NR-Clib/include/arch/AVR/BowlerConfig.h:
+
+../NR-Clib/include/arch/AVR/reg_structs.h:
+
+../NR-Clib/include/Bowler/BowlerServerHardwareAbstraction.h:
+
+include/DyIOApp/HardwareProfile.h:
+
+../common/include/DyIO/DyIO_def.h:
+
+../common/include/DyIO/Debug_DyIO.h:
+
+../common/include/DyIO/ChannelMode.h:
+
+../common/include/DyIO/ChannelValues.h:
+
+../NR-Clib/include/Bowler/AbstractPID.h:
+
+include/DyIOApp/Power.h:
+
+include/DyIOApp/Servo.h:
+
+include/DyIOApp/AVR_IO.h:
+
+include/DyIOApp/ADC.h:
+
+include/DyIOApp/PWM.h:
+
+include/DyIOApp/EEPROM.h:
+
+include/DyIOApp/UARTPassThrough.h:
+
+include/DyIOApp/DCMotorDriver.h:
+
+include/DyIOApp/Push.h:
+
+include/DyIOApp/SPISlave.h:
