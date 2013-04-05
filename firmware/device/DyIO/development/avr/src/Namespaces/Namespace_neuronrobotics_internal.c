6b67989c949c97c5b938821dbc1f61d50eeb70cb
Kevin Harrington <mad.hephaestus@gmail.com>
2013-04-05 12:40:46 +0000

    AVR compiles STILL BROKEN

--- firmware/device/DyIO/development/avr/src/Namespaces/Namespace_neuronrobotics_internal.c
+++ firmware/device/DyIO/development/avr/src/Namespaces/Namespace_neuronrobotics_internal.c
@@ -11,6 +11,7 @@ const char internalNSName[] = "neuronrobotics.internal*;0.3;;";
 RunEveryData pid={0,30};
 RunEveryData vel={0,100};
 
+static BYTE zone;
 BOOL bcsPidDypidAsyncEventCallback(BowlerPacket* Packet,BOOL (*pidAsyncCallbackPtr)(BowlerPacket *Packet)){
 	checkDigital();
 	checkAnalog();
@@ -19,8 +20,66 @@ BOOL bcsPidDypidAsyncEventCallback(BowlerPacket* Packet,BOOL (*pidAsyncCallbackP
 }
 
 BOOL bcsPidDypidProcessor_g(BowlerPacket * Packet){
-
+	zone = 4;
+	UINT16 i;
+	UINT16  start;
+	UINT16 end;
+	//UINT16_UNION adc;
 	switch (Packet->use.head.RPC){
+	case EEPD:
+		start = Packet->use.data[0];
+		end = Packet->use.data[1];
+		Packet->use.head.Method=BOWLER_POST;
+		if (end >= start){
+			for (i=start;i<end;i++){
+				Packet->use.data[i-start]=EEReadData(i);
+			}
+			Packet->use.head.DataLegnth= 4+end-start;
+		}else
+			ERR(Packet,zone,1);
+		break;
+	case _REV:
+		Packet->use.data[0]=MAJOR_REV;
+		Packet->use.data[1]=MINOR_REV;
+		Packet->use.data[2]=FIRMWARE_VERSION;
+		Packet->use.head.DataLegnth=4+3;
+		break;
+	case SAVE:
+		Packet->use.head.Method=BOWLER_POST;
+		Packet->use.head.DataLegnth = 4 + NUM_PINS;
+		for (i=0;i<NUM_PINS;i++){
+			Packet->use.data[i]=EEReadValue(i);
+		}
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
+BOOL bcsPidDypidProcessor_p(BowlerPacket * Packet){
+	zone = 5;
+	UINT16_UNION v;
+	UINT16 start;
+	UINT16  end;
+	UINT16 i;
+	switch (Packet->use.head.RPC){
+	case _PWR:
+		v.byte.SB = Packet->use.data[2];
+		v.byte.LB = Packet->use.data[3];
+		SetPowerState0(Packet->use.data[2],Packet->use.data[0]);
+		SetPowerState1(Packet->use.data[3],Packet->use.data[1]);
+		//Packet->use.head.MessageID=44;
+		break;
+	case EEPD:
+		start = Packet->use.data[0];
+		end = Packet->use.data[1];
+		if (end >= start){
+			for (i=0;i<(end-start);i++){
+				EEWriteData(i+start,Packet->use.data[i+2]);
+			}
+		}else
+			ERR(Packet,zone,4);
+		break;
 
 	default:
 		return FALSE;
@@ -31,7 +90,17 @@ BOOL bcsPidDypidProcessor_g(BowlerPacket * Packet){
 BOOL bcsPidDypidProcessor_c(BowlerPacket * Packet){
 
 	switch (Packet->use.head.RPC){
-
+	case _MAC:
+		if(ProvisionMAC(Packet->use.data)){
+			READY(Packet,zone,1);
+			break;
+		}
+		else
+			ERR(Packet,zone,1);
+		break;
+	case _PWR:
+		setPowerOverride(Packet->use.data[0]);
+		break;
 	default:
 		return FALSE;
 	}
