4e96ab2e99727dd7a0e9eceb2450ddddaa57c639
kharrington <kharrington@neuronrobotics.com>
2014-02-07 18:15:35 -0500

    Finished the namesapace sample
    The sample for the namespace example is filled in and working.

--- sample/SamplePic32MX795F512L/src/NamespaceSample.c
+++ sample/SamplePic32MX795F512L/src/NamespaceSample.c
@@ -1,4 +1,5 @@
 #include "Bowler/Bowler.h"
+#define setLed(a,b,c) 	_RD0=a;_RD1=b;_RD2=c;
 
 static const unsigned char cartNSName[]  = "bcs.sample.*;0.3;;";
 
@@ -10,14 +11,26 @@ BOOL sampleAsyncEventCallback(BowlerPacket * Packet,BOOL (*pidAsyncCallbackPtr)(
 }
 
 BOOL onSamplePost(BowlerPacket *Packet){
-    //this is the processor
-    
+
+    // get the values to use for the LED values
+    int val0 = get32bit( Packet, 0);
+    int val1 = get32bit( Packet, 4);
+    int val2 = get32bit( Packet, 8);
+
+    _RD0=val0?1:0;
+    _RD1=val1?1:0;
+    _RD2=val2?1:0;
+
+    // set the button values back into the packet
+    set32bit(Packet,_RD6, 0);
+    set32bit(Packet,_RD7, 4);
+    set32bit(Packet,_RD13, 8);
 }
 
 
 static RPC_LIST sample_TEST={	BOWLER_POST,// Method
                                 "TEST",//RPC as string
-                                &onSamplePost,//function pointer to a packet parsinf function
+                                &onSamplePost,//function pointer to a packet parsing function
                                 ((const char [4]){	BOWLER_I32,//
                                                         BOWLER_I32,//
 							BOWLER_I32,//
