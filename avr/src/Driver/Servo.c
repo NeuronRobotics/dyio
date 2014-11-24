ac4f2ac916485ce6ae9828b3fbb2392d95b50bea
madhephaestus <mad.hephaestus@gmail.com>
2014-11-24 17:36:48 -0500

    moved variables to the top

--- avr/src/Driver/Servo.c
+++ avr/src/Driver/Servo.c
@@ -30,6 +30,10 @@ void pinOff(uint8_t pin);
 
 boolean servoEngineStarted =false;
 
+uint8_t bOK[2]={false,false};
+uint8_t blockServo[2]={true,true};
+
+
 boolean getPowerOverRide(){
 	return powerOverRide;
 }
@@ -52,18 +56,9 @@ void setPowerOverride(boolean set){
 	println_W("powerOverRide: ");p_int_W(set);
 	powerOverRide = set?true:false;
 }
-uint8_t bOK[2]={false,false};
-//uint8_t b1OK=false;
-uint8_t blockServo[2]={true,true};
-//uint8_t b1lock=true;
-//uint8_t b0OK=false;
-//uint8_t b1OK=false;
-//uint8_t b0lock=true;
-//uint8_t b1lock=true;
+
 
 void SetPowerState(int bank,boolean railOk,boolean regulated){
-
-
 	bOK[bank]=false;
 	if (regulated == 1){
 		if(bank==0){
@@ -125,71 +120,10 @@ void SetServoPos(uint8_t pin,uint8_t val,float time){
 		return;
 	print = pin;
 
-//	if(pinServoOk(pin) == false){
-//		println_W("Servo\tchan: ");p_int_W(pin);
-//		print_W(" \tto val: ");p_int_W(val);
-//		print_W("\tin time: ");p_fl_W(time);
-//		if(pin > 11){
-//			print_W(" \tb1OK: ");p_int_W(b1OK);
-//			print_W(" \tpowerOverRide: ");p_int_W(powerOverRide);
-//			print_W(" \tb1lock: ");p_int_W(b1lock);
-//		}else{
-//			print_W(" \tb0OK: ");p_int_W(b0OK);
-//			print_W(" \tpowerOverRide: ");p_int_W(powerOverRide);
-//			print_W(" \tb0lock: ");p_int_W(b0lock);
-//		}
-//	}
-
 }
 uint8_t GetServoPos(uint8_t pin){
 	return getBcsIoDataTable(pin)->PIN.currentValue;
 }
-
-//void RunServo(uint8_t block){
-//
-//	return;//short circuit servo system
-//	//disableDebug();
-//	uint16_t j;
-//	uint8_t xIndex;
-//	//return;
-//	uint8_t start=block*BLOCK_SIZE;
-//	uint8_t stop=(block*BLOCK_SIZE)+BLOCK_SIZE;
-//
-//	runLinearInterpolationServo(start,stop);
-//	FlagBusy_IO=1;
-//	//Short delay to allow any stray transactions to finish
-//	_delay_us(800);
-//	uint8_t num=0;
-//	for (j=start;j<stop;j++){
-//		num+=pinOn(j);
-//	}
-//	//run minimal .75 ms pulse
-//	DelayPreServo();
-//	//loop 255 times and turn off all servos as their set position is equal to the loop counter
-//	for (j=0;j<256;j++){
-//		//check all servo positions
-//		for (xIndex=start; xIndex < stop ;xIndex++){
-//			if (j == getBcsIoDataTable(xIndex)->PIN.currentValue){
-//				//turn off if it is time to turn off
-//				pinOff(xIndex);
-//			}
-//		}
-//		// Small delay to pad the loop
-//		//_delay_us(1);
-//		//Nop();Nop();Nop();  Nop();Nop();Nop();  Nop();Nop();Nop();   Nop();Nop();
-//	}
-//	FlagBusy_IO=0;
-//	for (xIndex=start; xIndex < stop ;xIndex++){
-//			pinOff(xIndex);
-//	}
-//
-////	if(print!=0xff){
-////		print_I("\n\tset: ");p_fl_I(velocity[print].set);
-////		print_I("\n\tServoPos: ");p_int_I(DATA.PIN[print].ServoPos);
-////		print = 0xff;
-////	}
-//	//enableDebug();
-//}
 
 boolean pinServoOk(uint8_t pin){
 	int bank = (pin > 11)?1:0;
@@ -227,10 +161,6 @@ void pinOff(uint8_t pin){
 	}
 }
 
-//void DelayPreServo(void){
-//	volatile uint32_t _dcnt = 290;
-//	while (_dcnt--);
-//}
 
 void runLinearInterpolationServo(uint8_t blockStart,uint8_t blockEnd){
 	uint8_t i;
