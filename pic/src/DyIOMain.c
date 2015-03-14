3addad5c0293ae17fd21b45bd0639d93cae55797
madhephaestus <mad.hephaestus@gmail.com>
2015-03-14 17:45:08 -0400

    bowler server has been moved into ISR

--- pic/src/DyIOMain.c
+++ pic/src/DyIOMain.c
@@ -2,13 +2,13 @@
 #include "UserApp.h"
 
 void buttonCheck(uint8_t code){
-	if (_RB0==1){
-		p_int_E(code);print_E(" Reset");
-		SetColor(1,1,1);
-		U1CON = 0x0000;
-		DelayMs(100);
-		Reset();
-	}
+//	if (_RB0==1){
+//		p_int_E(code);print_E(" Reset");
+//		SetColor(1,1,1);
+//		U1CON = 0x0000;
+//		DelayMs(100);
+//		Reset();
+//	}
 
 }
 
@@ -23,17 +23,17 @@ void runDyIOMain(void){
 	UserInit();// User code init
 	//println_I("Main Loop Start");
 
-	OpenTimer4(T4_ON | T4_SOURCE_INT | T4_PS_1_256, (0x1000/10 ));
-	ConfigIntTimer3(T4_INT_ON | T4_INT_PRIOR_5);
+	OpenTimer4(T4_ON | T4_SOURCE_INT | T4_PS_1_256, 300);// .5ms
+	ConfigIntTimer4(T4_INT_ON | T4_INT_PRIOR_5);
 
 	while (1){
-
+		//Bowler_Server((BowlerPacket *) &Packet, false) ;
 		RunNamespaceAsync(&Packet,&PutBowlerPacket);
 		buttonCheck(0);
 	}
 }
 
-void __ISR(_TIMER_4_VECTOR, ipl5) _Timer3Handler(void)
+void __ISR(_TIMER_4_VECTOR, ipl5) _Timer4Handler(void)
 {
 	//StartCritical();
 	ConfigIntTimer4(T4_INT_OFF);
@@ -41,7 +41,12 @@ void __ISR(_TIMER_4_VECTOR, ipl5) _Timer3Handler(void)
 	// Run the Bowler Stack Namespace iteration of all async packets
 	// Pass in  the function pointer to push the packets upstream
 	Bowler_Server((BowlerPacket *) &Packet, false) ;
-	buttonCheck(99);
+	if (_RB0==1){
+		SetColor(0,1,1);
+		U1CON = 0x0000;
+		DelayMs(100);
+		Reset();
+	}
 	ConfigIntTimer4(T4_INT_ON | T4_INT_PRIOR_5);
 	//EndCritical();
 }
