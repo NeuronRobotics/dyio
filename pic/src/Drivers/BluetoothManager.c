d60582ae7e294fd8358b437b306ebcfc09399111
hephaestus <mad.hephaestus@gmail.com>
2015-07-30 14:38:55 -0400

    removing unused bluetooth code

--- pic/src/Drivers/BluetoothManager.c
+++ pic/src/Drivers/BluetoothManager.c
@@ -20,58 +20,58 @@ int getBluetoothBaud(){
 }
 
 void sendString(char * data){
-	Pic32UARTGetArray(packet,Pic32Get_UART_Byte_Count());
-	int i=0;
-	while(data[i++]!=0){
-		// calculate length of string stored in i
-	}
-	Pic32UARTPutArray((uint8_t *)data,i-1);
-
-	int tick = 2000;
-	while(Pic32Get_UART_Byte_Count()<2 && tick>0){
-		tick--;
-		DelayMs(1);
-		buttonCheck(100);
-	}
+//	Pic32UARTGetArray(packet,Pic32Get_UART_Byte_Count());
+//	int i=0;
+//	while(data[i++]!=0){
+//		// calculate length of string stored in i
+//	}
+//	Pic32UARTPutArray((uint8_t *)data,i-1);
+//
+//	int tick = 2000;
+//	while(Pic32Get_UART_Byte_Count()<2 && tick>0){
+//		tick--;
+//		DelayMs(1);
+//		buttonCheck(100);
+//	}
 
 }
 
 
 
 void configBluetooth(){
-	char name [] ="AT+NAMENR_DyIOxxxx\0";
-	//FlashGetMac(MyMAC.v);
-	name[14]=GetHighNib(MyMAC.v[4]);
-	name[15]=GetLowNib(MyMAC.v[4]);
-	name[16]=GetHighNib(MyMAC.v[5]);
-	name[17]=GetLowNib(MyMAC.v[5]);
-
-	sendString(name);
-	Pic32UARTGetArray(packet,Pic32Get_UART_Byte_Count());
-	if(!(packet[0]=='O' && packet[1]=='K')){
-		int i=0;
-		for(i=0;i<10;i++){
-			SetColor(i%2,i%2,i%2);//Set LED to white
-			DelayMs(100);
-		}
-	}
-	if(myBaud == 9600){
-		sendString("AT+BAUD9");
-		Pic32UARTGetArray(packet,Pic32Get_UART_Byte_Count());
-		if(packet[0]=='O' && packet[1]=='K'){
-			Pic32UARTSetBaud( HIGH_BAUD );
-			BluetoothReset(OFF); // Pull BT module out of reset
-			DelayMs(100);
-			BluetoothReset(ON); // Pull BT module out of reset
-			DelayMs(100 );//wait for it to settle
-		}else{
-			int i=0;
-			for(i=0;i<10;i++){
-				SetColor(i%2,0,0);//Set LED to red
-				DelayMs(100);
-			}
-		}
-	}
+//	char name [] ="AT+NAMENR_DyIOxxxx\0";
+//	//FlashGetMac(MyMAC.v);
+//	name[14]=GetHighNib(MyMAC.v[4]);
+//	name[15]=GetLowNib(MyMAC.v[4]);
+//	name[16]=GetHighNib(MyMAC.v[5]);
+//	name[17]=GetLowNib(MyMAC.v[5]);
+//
+//	sendString(name);
+//	Pic32UARTGetArray(packet,Pic32Get_UART_Byte_Count());
+//	if(!(packet[0]=='O' && packet[1]=='K')){
+//		int i=0;
+//		for(i=0;i<10;i++){
+//			SetColor(i%2,i%2,i%2);//Set LED to white
+//			DelayMs(100);
+//		}
+//	}
+//	if(myBaud == 9600){
+//		sendString("AT+BAUD9");
+//		Pic32UARTGetArray(packet,Pic32Get_UART_Byte_Count());
+//		if(packet[0]=='O' && packet[1]=='K'){
+//			Pic32UARTSetBaud( HIGH_BAUD );
+//			BluetoothReset(OFF); // Pull BT module out of reset
+//			DelayMs(100);
+//			BluetoothReset(ON); // Pull BT module out of reset
+//			DelayMs(100 );//wait for it to settle
+//		}else{
+//			int i=0;
+//			for(i=0;i<10;i++){
+//				SetColor(i%2,0,0);//Set LED to red
+//				DelayMs(100);
+//			}
+//		}
+//	}
 }
 
 boolean testAtCommand(int baud){
@@ -91,55 +91,56 @@ boolean testAtCommand(int baud){
 }
 
 uint8_t hasBluetooth(){
-#if defined(HAS_BLUTOOTH)
-	if(!btChecked){
-
-		SetColor(1,1,0);//Set LEd to yellow
-
-		/**
-		 * Start by checking the possible baudrates
-		 */
-		int i;
-		for(i=0;i<(sizeof(bauds)/sizeof(int));i++){
-			if(btOk==false ){
-				if(testAtCommand(bauds[i])){
-					btOk = true; 
-					myBaud  = bauds[i];
-					//if(myBaud == 9600){
-						configBluetooth();
-					//}
-				}
-			}
-		}
-
-
-		if(!btOk){
-			int i=0;
-			for(i=0;i<10;i++){
-				SetColor(i%2,0,i%2);//Set LED to purple
-				DelayMs(100);
-			}
-		}
-
-		btChecked = true; 
-	}
-	return btOk;
-#else
-	return false; 
-#endif
+//#if defined(HAS_BLUTOOTH)
+//	if(!btChecked){
+//
+//		SetColor(1,1,0);//Set LEd to yellow
+//
+//		/**
+//		 * Start by checking the possible baudrates
+//		 */
+//		int i;
+//		for(i=0;i<(sizeof(bauds)/sizeof(int));i++){
+//			if(btOk==false ){
+//				if(testAtCommand(bauds[i])){
+//					btOk = true;
+//					myBaud  = bauds[i];
+//					//if(myBaud == 9600){
+//						configBluetooth();
+//					//}
+//				}
+//			}
+//		}
+//
+//
+//		if(!btOk){
+//			int i=0;
+//			for(i=0;i<10;i++){
+//				SetColor(i%2,0,i%2);//Set LED to purple
+//				DelayMs(100);
+//			}
+//		}
+//
+//		btChecked = true;
+//	}
+//	return btOk;
+//#else
+//	return false;
+//#endif
+    return false;
 }
 
 
 void initBluetooth(){
-#if defined(HAS_BLUTOOTH)
-	btChecked = false; 
-	mPORTDOpenDrainClose(BIT_1 | BIT_2 | BIT_3); // make sure the com port is driven 3.3
-	BluetoothResetTRIS (); //output mode on reset line
-	BluetoothCommandTRIS (); //output mode on CMD line
-	BluetoothReset(ON); // Pull BT module out of reset
-	DelayMs(BT_RESET_DELAY );//wait for it to settle
-	if(!hasBluetooth()){
-		Pic32UARTSetBaud( 115200 );
-	}
-#endif
+//#if defined(HAS_BLUTOOTH)
+//	btChecked = false;
+//	mPORTDOpenDrainClose(BIT_1 | BIT_2 | BIT_3); // make sure the com port is driven 3.3
+//	BluetoothResetTRIS (); //output mode on reset line
+//	BluetoothCommandTRIS (); //output mode on CMD line
+//	BluetoothReset(ON); // Pull BT module out of reset
+//	DelayMs(BT_RESET_DELAY );//wait for it to settle
+//	if(!hasBluetooth()){
+//		Pic32UARTSetBaud( 115200 );
+//	}
+//#endif
 }
